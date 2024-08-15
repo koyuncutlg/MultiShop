f8bd9b9889697a029016e67e3e3482b20c6d854d
koyuncutlg <tolga_koyuncu@hotmail.com>
2024-08-15 16:07:14 +0300

    devams

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductDetailServices/ProductDetailService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductDetailServices
+{
+	public class ProductDetailService : IProductDetailService
+	{
+		private readonly IMongoCollection<ProductDetail> _ProductDetailCellection;
+		private readonly IMapper _mapper;
+
+		public ProductDetailService(IMapper mapper, IDatabaseSettings _databaseSettings)
+		{
+			var client = new MongoClient(_databaseSettings.ConnectionString);
+			var database = client.GetDatabase(_databaseSettings.DatabaseName);
+			_ProductDetailCellection = database.GetCollection<ProductDetail>(_databaseSettings.ProductDetialCollectionName);
+			_mapper = mapper;
+		}
+		public async Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto)
+		{
+			var values = _mapper.Map<ProductDetail>(createProductDetailDto);
+			await _ProductDetailCellection.InsertOneAsync(values);
+		}
+
+		public async Task DeleteProductDetailAsync(string id)
+		{
+			await _ProductDetailCellection.DeleteOneAsync(x => x.ProductDetailID == id);
+		}
+
+		public async Task<List<ResultProductDetailDto>> GetAllProductDetailAsync()
+		{
+			var values = await _ProductDetailCellection.Find(x => true).ToListAsync();
+			return _mapper.Map<List<ResultProductDetailDto>>(values);
+		}
+
+		public async Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(string id)
+		{
+			var values = await _ProductDetailCellection.Find<ProductDetail>(x => x.ProductDetailID == id).FirstOrDefaultAsync();
+			return _mapper.Map<GetByIdProductDetailDto>(values);
+		}
+
+		public async Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto)
+		{
+			var values = _mapper.Map<ProductDetail>(updateProductDetailDto);
+			await _ProductDetailCellection.FindOneAndReplaceAsync(x => x.ProductDetailID == updateProductDetailDto.ProductDetailID, values);
+		}
+	}
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/ProductImageService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Entities; 
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+	public class ProductImageService : IProductImageService
+	{
+		private readonly IMongoCollection<ProductImage> _ProductImageCellection;
+		private readonly IMapper _mapper;
+
+		public ProductImageService(IMapper mapper, IDatabaseSettings _databaseSettings)
+		{
+			var client = new MongoClient(_databaseSettings.ConnectionString);
+			var database = client.GetDatabase(_databaseSettings.DatabaseName);
+			_ProductImageCellection = database.GetCollection<ProductImage>(_databaseSettings.ProductDetialCollectionName);
+			_mapper = mapper;
+		}
+		public async Task CreateProductImageAsync(CreateProductImageDto createProductImageDto)
+		{
+			var values = _mapper.Map<ProductImage>(createProductImageDto);
+			await _ProductImageCellection.InsertOneAsync(values);
+		}
+
+		public async Task DeleteProductImageAsync(string id)
+		{
+			await _ProductImageCellection.DeleteOneAsync(x => x.ProductImagesID == id);
+		}
+
+		public async Task<List<ResultProductImageDto>> GetAllProductImageAsync()
+		{
+			var values = await _ProductImageCellection.Find(x => true).ToListAsync();
+			return _mapper.Map<List<ResultProductImageDto>>(values);
+		}
+
+		public async Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id)
+		{
+			var values = await _ProductImageCellection.Find<ProductImage>(x => x.ProductImagesID == id).FirstOrDefaultAsync();
+			return _mapper.Map<GetByIdProductImageDto>(values);
+		}
+
+		public async Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto)
+		{
+			var values = _mapper.Map<ProductImage>(updateProductImageDto);
+			await _ProductImageCellection.FindOneAndReplaceAsync(x => x.ProductImagesID == updateProductImageDto.ProductImagesID, values);
+		}
+	}
+}
