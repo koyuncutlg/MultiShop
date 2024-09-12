881704dbdb7d65655b0d2b60e281cf9fa435b7b5
koyuncutlg <tolga_koyuncu@hotmail.com>
2024-09-12 16:58:25 +0300

    102

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoOperationsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoOperationDto;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CargoOperationsController : ControllerBase
+	{
+		private readonly ICargoOperationService _CargoOperationService;
+
+		public CargoOperationsController(ICargoOperationService CargoOperationService)
+		{
+			_CargoOperationService = CargoOperationService;
+		}
+		[HttpGet]
+		public IActionResult CargoOperationList()
+		{
+			var values = _CargoOperationService.TGetAll();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateCargoOperation(CreateCargoOperationDto createCargoOperationDto)
+		{
+			CargoOperation CargoOperation = new CargoOperation()
+			{
+				Barcode = createCargoOperationDto.Barcode,
+				Description = createCargoOperationDto.Description,
+				OperationDate = createCargoOperationDto.OperationDate
+			};
+			_CargoOperationService.TInsert(CargoOperation);
+			return Ok("Kargo hareketi başarıyla oluşturuldu!");
+		}
+
+		[HttpDelete]
+		public IActionResult RemoveCargoOperation(int id)
+		{
+			_CargoOperationService.TDelete(id);
+			return Ok("Kargo hareketi başarıyla silindi!");
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetCargoOperationById(int id)
+		{
+			var values = _CargoOperationService.TGetById(id);
+			return Ok(values);
+		}
+		[HttpPut]
+		public IActionResult UpdateCargoOperation(UpdateCargoOperationDto updateCargoOperationDto)
+		{
+			CargoOperation CargoOperation = new CargoOperation()
+			{
+				CargoOperationId = updateCargoOperationDto.CargoOperationId,
+				Barcode = updateCargoOperationDto.Barcode,
+				Description = updateCargoOperationDto.Description,
+				OperationDate = updateCargoOperationDto.OperationDate
+			};
+			_CargoOperationService.TUpdate(CargoOperation);
+			return Ok("Kargo hareketi başarıyla güncellendi!");
+		}
+	}
+}
