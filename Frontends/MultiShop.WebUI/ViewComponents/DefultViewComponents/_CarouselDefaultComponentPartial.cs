103bb6495d71b9059594dd5075047a9cd70d9407
koyuncutlg <tolga_koyuncu@hotmail.com>
2024-09-13 15:28:33 +0300

    124

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefultViewComponents
+{
+    public class _CarouselDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
